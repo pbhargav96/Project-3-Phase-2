7355fa14068d0bedd0e2c5834f209005f2aad205
AmoghavarshaP <60764108+AmoghavarshaP@users.noreply.github.com>
2020-03-18 23:39:35 -0400

    Update A-Star Algorithm converted from Dijkstra

--- A_main.py
+++ A_main.py
@@ -121,3 +121,82 @@ def coll_check(position,crad):
 		return True
 	else:
 		return False
+	
+#Update A Star Algorithm from dijkstra and path generation
+def priority_pop(queue):  # Priority Queue, outputs the node with least cost attached to it
+    min_a = 0
+    for elemt in range(len(queue)):
+        if queue[elemt].cost < queue[min_a].cost:
+            min_a = elemt
+    return queue.pop(min_a)
+
+def find_node(point, queue):
+    for elem in queue:
+        if elem.state == point:
+            return queue.index(elem)
+        else:
+            return None
+
+def track_back(node):
+    print("Tracking Back")
+    p = []
+    p.append(node.parent)
+    parent = node.parent
+    if parent is None:
+        return p
+    while parent is not None:
+        p.append(parent)
+        parent = parent.parent
+    return p
+
+def color_pixel(image_color, point):
+    #print(point)
+    image_color[point[1], point[0]] = [255,0,255]
+    return image_color
+
+def CostToGoal(point,goal):
+    xp = point[0]
+    yp = point[1]
+    xg = goal[0]
+    yg = goal[1]
+    euclidean_distance= np.sqrt((xp-xg)**2 + (yp-yg)**2)
+    return euclidean_distance
+def AStar_Algo(start,goal,imap,crad):
+	visited = []
+	s_node = Nodes(start)
+	s_node.cost = 0
+	Q = [s_node]
+	imap[start[1], start[0]] = [255, 0, 0]
+	imap[goal[1], goal[0]] = [0, 0, 255]
+	while Q:
+		current = priority_pop(Q)
+		visited.append(current.state)
+		neighbors = mappingofneighbors(current.state,crad)
+
+		for n in neighbors:
+			if n[0] is not None:
+
+				new_node = Nodes(n[0])
+				#print(n[0])
+				new_node.parent = current
+
+				if n[0][0]==goal[0] and n[0][1]==goal[1]:
+					print("Goal Reached")
+					return new_node,imap,visited
+
+				if n[0] not in visited:
+					new_node.cost = n[1]+new_node.parent.cost+CostToGoal(n[0],goal)
+					visited.append(new_node.state)
+					Q.append(new_node)
+				else:
+					node_id = find_node(new_node,Q)
+
+					if node_id:
+						t_node = queue[node_id]
+						if t_node.cost> n[1]+new_node.parent.cost+CostToGoal(n[0],goal):
+							t_node.cost= n[1]+new_node.parent.cost+CostToGoal(n[0],goal)
+							t_node.parent = current
+			else:
+				continue
+
+	return None,None,None
